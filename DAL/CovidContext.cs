616e7b4d5ea3c58dbec13756291cbed479e29b90
Carlos Blanco <acbgalan@gmail.com>
2020-11-30 00:38:33 +0100

    Se añade contexto y cadena de conexión para manejar la BD donde se alojaran los cuestionarios.

---
+++ DAL/CovidContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Covid19.DAL.Models;
+
+namespace Covid19.DAL
+{
+    public class CovidContext : DbContext
+    {
+        public CovidContext() : base("COVID-19") { }
+
+        public DbSet<Cuestionario> Cuestionarios { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
